03cf640e0a6e8cf9243cc2895c547add24a15414
Lucas <lucas.silva@agencian1.com.br>
2023-08-17 01:06:34 -0300

    Start na criação da screen novo elemento

--- src/screens/AddNewElement.js
+++ src/screens/AddNewElement.js
@@ -1,10 +1,53 @@
-import { SafeAreaView, Text } from "react-native";
+import { SafeAreaView, TextInput, View, Text, KeyboardAvoidingView } from "react-native";
+import Checkbox from "expo-checkbox";
 import styles from "../styles/screens/AddNewElement";
 
-const AddNewElement = ()=>{
-    return(
+const AddNewElement = () => {
+    return (
         <SafeAreaView style={styles.container}>
-            <Text>Apenas um teste 2 Auiuuuuuuuu</Text>
+            <View>
+                <Text>Nome do Elemento</Text>
+                <TextInput />
+            </View>
+            <View>
+                <Text>Horas disponíveis</Text>
+                <TextInput />
+            </View>
+            <View>
+                <View>
+                    <Text>Selecione os dias disponíveis</Text>
+                </View>
+                <View>
+                    <View>
+                        <Checkbox />
+                        <Text>Segunda</Text>
+                    </View>
+                    <View>
+                        <Checkbox />
+                        <Text>Terça</Text>
+                    </View>
+                    <View>
+                        <Checkbox />
+                        <Text>Quarta</Text>
+                    </View>
+                    <View>
+                        <Checkbox />
+                        <Text>Quinta</Text>
+                    </View>
+                    <View>
+                        <Checkbox />
+                        <Text>Sexta</Text>
+                    </View>
+                    <View>
+                        <Checkbox />
+                        <Text>Sabado</Text>
+                    </View>
+                    <View>
+                        <Checkbox />
+                        <Text>Domingo</Text>
+                    </View>
+                </View>
+            </View>
         </SafeAreaView>
     );
 }
